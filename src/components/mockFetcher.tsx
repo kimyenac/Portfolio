9b1a3d4e0b8aa7a27b5a4b2584223bf423f37304
kimyenac <yena.c.2005@gmail.com>
2022-12-28 23:32:44 +0900

    fix: mockFetcher 반환값 에러 해결

--- src/components/mockFetcher.tsx
+++ src/components/mockFetcher.tsx
@@ -1,9 +1,7 @@
 /* mock 데이터 가져오는 Fetcher */
 const mockFetcher = (key: string) => {
-  const data = fetch(`/mock${key}`)
-    .then((v) => v.json())
-    .catch((e) => console.error(e));
-  return data;
+  const result = fetch(`/mock${key}`).then((v) => v.json());
+  return result.then((v) => v.data);
 };
 
 export default mockFetcher;
